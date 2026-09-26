9c0f53fd9b3d9887d7e82712d28c8c6997ecd146
Li-Brunestud <97831667+Li-Brunestud@users.noreply.github.com>
2026-09-26 20:42:55 +0800

    Add day11 object collection exercises

---
+++ day11-object-collections/day11-object-collections/day11-object-collections.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <vector>
+class Player
+{
+private:
+    std::string name;
+	int hp;
+public:
+	Player(std::string name, int hp) : name(name), hp(hp) {}
+	std::string getName() const 
+    { 
+        return name; 
+    }
+    int getHp() const
+    {
+        return hp;
+    }
+    void takeDamage(int damage)
+    {
+		hp -= damage;
+    }
+};
+Player* findPlayer(std::vector<Player>& players, const std::string& targetName)
+{
+    for (Player& player : players)
+    {
+        if (player.getName() == targetName)
+        {
+            return &player;
+        }
+    }
+
+    return nullptr;
+}
+int main()
+{
+    std::vector<Player> players;
+    players.emplace_back("Leo", 100);
+    players.emplace_back("Li", 80);
+    players.emplace_back("Alice", 120);
+	std::string targetName = "Li";
+    Player* foundPlayer = findPlayer(players, targetName);
+    if (foundPlayer != nullptr)
+    {
+        foundPlayer->takeDamage(10);
+    }
+    return 0;
+}
